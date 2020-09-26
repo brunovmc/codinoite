31073b5bc88e75215bdeb545782b1c8951f3739b
Bruno Vidigal de magalhaes coriolano <bvidigal@c1r1p4.42sp.org.br>
2020-09-25 22:25:21 -0300

    arrumando a norma

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,31 +1,31 @@
 #include "get_next_line.h"
 
-int get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-    static char *s_line;
-    char        *buffer;
-    int         result;
+   	static char *s_line;
+	char        *buffer;
+	int         result;
 
-    if (fd < 0 || BUFFER_SIZE < 1 || !line)
-	    return (-1);
+	if (fd < 0 || BUFFER_SIZE < 1 || !line)
+		return (-1);
 	result = 1;
 	*line = NULL;
 	buffer = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-    if (!(s_line))
-        s_line = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-    while (isbarran(s_line) == -1  && result >= 1 && result <= BUFFER_SIZE)
-    {
-        result = read(fd, buffer, BUFFER_SIZE);
-        if (result >= 1 && result <= BUFFER_SIZE && s_line)
-            s_line = ft_strjoin(s_line, buffer);
-        ft_bzero(buffer);
-    }
-    if (result >= 0 && result <= BUFFER_SIZE)
-       s_line = cleanline(line, s_line);
+	if (!(s_line))
+		s_line = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	while (isbarran(s_line) == -1  && result >= 1 && result <= BUFFER_SIZE)
+	{
+		result = read(fd, buffer, BUFFER_SIZE);
+		if (result >= 1 && result <= BUFFER_SIZE && s_line)
+			s_line = ft_strjoin(s_line, buffer);
+		ft_bzero(buffer);
+   	}
+	if (result >= 0 && result <= BUFFER_SIZE)
+		s_line = cleanline(line, s_line);
 	free(buffer);
-    if (result >= 1 && result <= BUFFER_SIZE)
-        return (1);
-    return (result == 0 ? 0 : -1);
+	if (result >= 1 && result <= BUFFER_SIZE)
+		return (1);
+	return (result == 0 ? 0 : -1);
 }
 
 char *cleanline(char **line, char *s_line)
@@ -39,7 +39,7 @@ char *cleanline(char **line, char *s_line)
         s_line[pos] = '\0';
         *line = ft_calloc(pos + 1, sizeof(char));
         tmp = ft_calloc(ft_strlen(&s_line[pos + 1]) + 1, sizeof(char));
-        ft_strlcpy(*line, s_line, pos + 1); //adicionamos + 1 no pos por conta do indice
+        ft_strlcpy(*line, s_line, pos + 1);
         ft_strlcpy(tmp, &s_line[pos + 1], ft_strlen(&s_line[pos + 1]) + 1);
         free(s_line);
         s_line = NULL;
