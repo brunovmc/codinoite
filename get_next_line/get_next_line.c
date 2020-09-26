1d73a4533a103720c08a7f4ad89c2bd5d528bd27
brunovmc <bruno.vidigal.coriolano@gmail.com>
2020-09-25 22:15:31 -0300

    commit pra testar

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,5 +1,4 @@
 #include "get_next_line.h"
-#include <stdio.h>
 
 int get_next_line(int fd, char **line)
 {
@@ -17,18 +16,13 @@ int get_next_line(int fd, char **line)
     while (isbarran(s_line) == -1  && result >= 1 && result <= BUFFER_SIZE)
     {
         result = read(fd, buffer, BUFFER_SIZE);
-		// printf("Do GNL: linha lida: %s\n", buffer);
         if (result >= 1 && result <= BUFFER_SIZE && s_line)
-		{
             s_line = ft_strjoin(s_line, buffer);
-			// printf("Do GNL:linha lida: %s\n", s_line);
-		}
         ft_bzero(buffer);
     }
     if (result >= 0 && result <= BUFFER_SIZE)
        s_line = cleanline(line, s_line);
 	free(buffer);
-	// printf("Do GNL:result: %s\n", s_line);
     if (result >= 1 && result <= BUFFER_SIZE)
         return (1);
     return (result == 0 ? 0 : -1);
@@ -58,27 +52,6 @@ char *cleanline(char **line, char *s_line)
 	return (s_line);
 }
 
-size_t    ft_strlcpy(char *dst, char *src, size_t dstsize)
-{
-    size_t    i;
-    size_t    srclen;
-
-    if (dst == 0 || src == 0)
-        return (0);
-    i = 0;
-    srclen = ft_strlen(src);
-    if (dstsize)
-    {
-        while (src[i] && i < dstsize - 1)
-        {
-            dst[i] = src[i];
-            i++;
-        }
-        dst[i] = 0;
-    }
-    return (srclen);
-}
-
 int isbarran(char *string)
 {
     int i;
@@ -93,77 +66,4 @@ int isbarran(char *string)
         i++;
     }
     return (-1);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char			*str;
-	int	i;
-	int	j;
-
-	i = ft_strlen(s1);
-	j = ft_strlen(s2);
-	str = (char *)ft_calloc(i + j + 1, sizeof(char));
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	free(s1);
-	return (str);
-}
-
-size_t	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str)
-	{
-		while (str[i] != '\0')
-			i++;
-	}
-	return (i);
-}
-
-char	*ft_calloc(size_t count, size_t size)
-{
-	char		*str;
-	size_t		i;
-
-	if (count > 1000000)
-		count = 1000000;
-	str = (char *)malloc(count * size);
-	i = 0;
-	if (str)
-	{
-		while (i < count)
-		{
-			str[i] = '\0';
-			i++;
-		}
-	}
-	return (str);
-}
-
-void	ft_bzero(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str)
-	{
-		while (i < BUFFER_SIZE && i < 1000000)
-		{
-			str[i] = '\0';
-			i++;
-		}
-	}
 }
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,93 @@
+size_t    ft_strlcpy(char *dst, char *src, size_t dstsize)
+{
+    size_t    i;
+    size_t    srclen;
+
+    if (dst == 0 || src == 0)
+        return (0);
+    i = 0;
+    srclen = ft_strlen(src);
+    if (dstsize)
+    {
+        while (src[i] && i < dstsize - 1)
+        {
+            dst[i] = src[i];
+            i++;
+        }
+        dst[i] = 0;
+    }
+    return (srclen);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char			*str;
+	int	i;
+	int	j;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	str = (char *)ft_calloc(i + j + 1, sizeof(char));
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	free(s1);
+	return (str);
+}
+
+size_t	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str)
+	{
+		while (str[i] != '\0')
+			i++;
+	}
+	return (i);
+}
+
+char	*ft_calloc(size_t count, size_t size)
+{
+	char		*str;
+	size_t		i;
+
+	if (count > 1000000)
+		count = 1000000;
+	str = (char *)malloc(count * size);
+	i = 0;
+	if (str)
+	{
+		while (i < count)
+		{
+			str[i] = '\0';
+			i++;
+		}
+	}
+	return (str);
+}
+
+void	ft_bzero(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str)
+	{
+		while (i < BUFFER_SIZE && i < 1000000)
+		{
+			str[i] = '\0';
+			i++;
+		}
+	}
+}
