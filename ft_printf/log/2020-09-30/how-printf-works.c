99f162b7f3dd79970ec39e49d48004467339f628
Herbert Castanha <hcastanha@Herberts-MacBook-Air.local>
2020-09-30 22:48:21 -0300

    understanding how printf returns its int

---
+++ ft_printf/log/2020-09-30/how-printf-works.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	i = printf("8 letras");
+	printf("\n%d\n", i);
+	i = printf("13cd caracteres");
+	printf("\n%d\n", i);
+	i = printf("Uma frase com 27 caracteres");
+	printf("\n%d\n", i);
+	i = printf("Outra frase com 47 caracteres e um ponto final.");
+	printf("\n%d\n", i);
+
+}
